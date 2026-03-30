06cebc17b58601e8bdbaa4d9add56d4dbd296fb3
Chamod Champika <Chamod.Champika@gopl.com>
2026-03-30 17:56:42 +0530

    Update OneSignal REST API Key to newest provided version

--- app/api/notify/route.ts
+++ app/api/notify/route.ts
@@ -12,7 +12,7 @@ export async function POST(request: Request) {
   }
 
   const appId = process.env.ONESIGNAL_APP_ID || "cd58321e-12f6-4a62-8301-cc049ece1b1a";
-  const restApiKey = process.env.ONESIGNAL_REST_API_KEY || "os_v2_app_zvmdehqs6zfgfaybzqcj5tq3dk26n5n4ozreyqe4kwhsg3grreglu6xayhrisj62z3u5k6a2fiolvtggbuteu4c44z2aqowarfrt6wi";
+  const restApiKey = process.env.ONESIGNAL_REST_API_KEY || "os_v2_app_zvmdehqs6zfgfaybzqcj5tq3dkyjtlmiydqenhnbyw33knd2c3gy25elpgwi6f7aam2r7d3pgayo5rectmhddfg3vn64cpsv4gatsty";
 
   const data = {
     app_id: appId,
